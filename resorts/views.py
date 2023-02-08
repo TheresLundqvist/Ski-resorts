33f31097b3f1ccaefa554e28462b1e448a33f779
TheresLundqvist <theres.lundqvist@outlook.com>
2023-02-08 09:33:01 +0000

    Create list view for Resorts

--- resorts/views.py
+++ resorts/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Resort
 
-# Create your views here.
+
+class ResortList(generic.ListView):
+    model = Resort
+    queryset = Resort.objects.filter(status=1).order_by('-created_on')
+    template_name = 'index.html'
+    paginate_by = 6
+
