0f262f8ebda180e5d6de50bc633cd20a23faa242
TheresLundqvist <theres.lundqvist@outlook.com>
2023-02-20 13:40:11 +0000

    Style logo

--- resorts/urls.py
+++ resorts/urls.py
@@ -1,7 +1,10 @@
 from . import views
 from django.urls import path
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 
 urlpatterns = [
     path('', views.ResortList.as_view(), name='home'),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
