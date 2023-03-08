2c7a8317dedceb3c834b423e44bf3e311bd38624
TheresLundqvist <theres.lundqvist@outlook.com>
2023-03-08 20:23:48 +0000

    Add resort details to view

--- resorts/urls.py
+++ resorts/urls.py
@@ -5,6 +5,7 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('', views.ResortList.as_view(), name='home'),
+    path('<slug:slug>/', views.ResortDetail.as_view(), name='resort_detail'),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
--- resorts/views.py
+++ resorts/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 from .models import Resort
 
 
@@ -9,3 +9,19 @@ class ResortList(generic.ListView):
     template_name = 'index.html'
     paginate_by = 6
 
+
+class ResortDetail(View):
+
+    def get(self, request, slug, *args, **kwargs):
+        queryset = Resort.objects.filter(status=1)
+        resort = get_object_or_404(queryset, slug=slug)
+        comments = resort.comments.filter(approved=True).order_by("-created_on")  # noqa
+
+        return render(
+            request,
+            "resort_detail.html",
+            {
+                "resort": resort,
+                "comments": comments
+            },
+        )
