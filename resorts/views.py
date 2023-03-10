714d9152b7194541cec21f839b1f8c164f28088e
TheresLundqvist <theres.lundqvist@outlook.com>
2023-03-10 18:42:50 +0000

    Add rating and comments section

--- resorts/views.py
+++ resorts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic, View
-from .models import Resort, Rating
+from .models import Resort, Rating, Comment, Contact
 
 
 class ResortList(generic.ListView):
