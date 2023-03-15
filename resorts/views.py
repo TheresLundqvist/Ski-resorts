ee4bb1ea2560f3bcdcd65a9e9b54f99c3758b938
TheresLundqvist <theres.lundqvist@outlook.com>
2023-03-15 19:37:01 +0000

    Add counter for rating and comments

--- resorts/views.py
+++ resorts/views.py
@@ -16,12 +16,14 @@ class ResortDetail(View):
         queryset = Resort.objects.filter(status=1)
         resort = get_object_or_404(queryset, slug=slug)
         comments = resort.comments.filter(approved=True).order_by("-created_on")  # noqa
+        rating = resort.average_rating
 
         return render(
             request,
             "resort_detail.html",
             {
                 "resort": resort,
-                "comments": comments
+                "comments": comments,
+                "rating": rating,
             },
         )
