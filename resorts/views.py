d264d486210757b33823f3ee2ff960995400bff5
TheresLundqvist <theres.lundqvist@outlook.com>
2023-03-20 16:38:38 +0000

    Add comment form

--- resorts/views.py
+++ resorts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic, View
 from .models import Resort, Rating, Comment, Contact
+from .forms import CommentForm
 
 
 class ResortList(generic.ListView):
@@ -25,5 +26,6 @@ class ResortDetail(View):
                 "resort": resort,
                 "comments": comments,
                 "rating": rating,
+                "comment_form": CommentForm()
             },
         )
