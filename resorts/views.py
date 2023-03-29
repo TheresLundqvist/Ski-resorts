0544b5bc8914da998e9706d5df92c585b68ecacf
TheresLundqvist <theres.lundqvist@outlook.com>
2023-03-29 17:28:21 +0000

    Style comment section

--- resorts/views.py
+++ resorts/views.py
@@ -25,6 +25,36 @@ class ResortDetail(View):
             {
                 "resort": resort,
                 "comments": comments,
+                "commented": False,
+                "rating": rating,
+                "comment_form": CommentForm()
+            },
+        )
+
+    def post(self, request, slug, *args, **kwargs):
+        queryset = Resort.objects.filter(status=1)
+        resort = get_object_or_404(queryset, slug=slug)
+        comments = resort.comments.filter(approved=True).order_by("created_on")  # noqa
+        rating = resort.average_rating
+
+        comment_form = CommentForm(data=request.POST)
+
+        if comment_form.is_valid():
+            comment_form.instance.email = request.user.email
+            comment_form.instance.name = request.user.username
+            comment = comment_form.save(commit=False)
+            comment.resort = resort
+            comment.save()
+        else:
+            comment_form = CommentForm()
+
+        return render(
+            request,
+            "resort_detail.html",
+            {
+                "resort": resort,
+                "comments": comments,
+                "commented": True,
                 "rating": rating,
                 "comment_form": CommentForm()
             },
