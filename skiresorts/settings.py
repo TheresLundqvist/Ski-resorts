5e8f1bb4d9d4ecf4e8be6d388680585179dbfe0a
TheresLundqvist <theres.lundqvist@outlook.com>
2023-03-31 14:47:02 +0000

    Fix missing cloudinary installed app

--- skiresorts/settings.py
+++ skiresorts/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', False)
+DEBUG = False
 
 ALLOWED_HOSTS = ['ski-resorts-locator.herokuapp.com', 'localhost']
 
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.sites',
+    'cloudinary_storage',
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
